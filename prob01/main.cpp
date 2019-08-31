5f908dddc5d2cd91900c24358773521648cd163b
riften <riften@cs104u-6d.acad.fullerton.edu>
2019-08-31 14:55:07 -0700

    added diamond pattern

--- prob01/main.cpp
+++ prob01/main.cpp
@@ -1,10 +1,17 @@
-// Name: 
+// Name: Annie Van
 // This program displays a diamond pattern to the screen.
 
 #include <iostream>
 
+using namespace std;
 int main()
 {
-
+  cout << "    *"
+  cout << "   ***"
+  cout << "  *****"
+  cout << " *******"
+  cout << "  *****"
+  cout << "   ***"
+  cout << "    *"
   return 0;
 }
